c1104fcdd6a640df6c816a05d3569ed65e79fb48
Beytullah BİLEK <b.bilek_ktu@outlook.com>
2022-07-04 19:09:33 +0300

    Update LoopsPractice.java

--- src/LoopsPractice.java
+++ src/LoopsPractice.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class LoopsPractice {
-
+/*Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri
+ kabul eden ve girilen değerlerden çift ve 4'ün katları olan
+ sayıları toplayıp ekrana basan programı yazıyoruz.
+ */
     public static void main(String[] args) {
 
         int val, sum=0;
