c71026cbdd1352d0168a6da9dab3f605400222b5
Beytullah BİLEK <b.bilek_ktu@outlook.com>
2022-07-13 14:22:42 +0300

    Create DrawBWithArray.java

---
+++ src/DrawBWithArray.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class DrawBWithArray {
+
+    public static void main(String[] args) {
+        String[][] array = new String[7][4];
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array[i].length; j++) {
+                if(i==0){
+                    array[i][j]="* ";
+                }
+                else if (j==0){
+                    array[i][j]="* ";
+                }
+                else if (j==3){
+                    array[i][j]="* ";
+                }
+                else if (i==6){
+                    array[i][j]="* ";
+                }
+                else if (i==3){
+                    array[i][j]="* ";
+                }else{
+                    array[i][j]="  ";
+                }
+            }
+        }
+        for (String[] row : array){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
